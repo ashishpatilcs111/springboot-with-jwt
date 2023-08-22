5941f92740078badbdd9d9c2b46e162ab859c263
Ashish Patil <ashishpatilcs111@gmail.com>
2023-08-22 10:35:34 +0530

    all files uploaded

--- src/main/java/com/boot/controller/GlobalExceptionHandlingController.java
+++ src/main/java/com/boot/controller/GlobalExceptionHandlingController.java
@@ -1,16 +1,11 @@
 package com.boot.controller;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandlingController {
